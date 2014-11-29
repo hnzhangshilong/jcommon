1e202cf0d7ac8e6f3ca9de3019111138fd84821d
jack <jack_zsl@163.com>
2014-11-29 11:16:15 +0800

    公共头文件

---
+++ Public.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Public.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/29/2014 10:17:35 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  张世龙 (mn), jack_zsl@163.com
+ *        Company:  free
+ *
+ * =====================================================================================
+ */
+#ifndef GIT_JCOMMON_PUBLIC_H
+#define GIT_JCOMMON_PUBLIC_H
+
+#include <stdio.h>
+#include <assert.h>
+
+#define SUCCESS 0
+#define ERROR -1
+#define TRUE 1
+#define FALSE 0
+
+#endif
